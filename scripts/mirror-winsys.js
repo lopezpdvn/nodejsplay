b46c0c4ddad74bb12cc29f1e8b1d813f1e2ca011
Pedro Ivan Lopez <m@pedroivanlopez.com>
2015-08-14 00:55:20 -0500

    Using syspol policy, read environment variables

--- scripts/mirror-winsys.js
+++ scripts/mirror-winsys.js
@@ -7,9 +7,32 @@ require('shelljs/global');
 var fs   = require('fs');
 var child_process = require('child_process');
 var spawn = child_process.spawnSync;
+var PATH_SEPARATOR = ';';
 
-var rsync = exec('rsync -v');
-console.log(rsync.output);
+srcPathsEnvKey = "SYSPOL_EXTERNAL_MIRROR_BACKUP_SOURCE_PATH";
+dstPathsEnvKey = "SYSPOL_EXTERNAL_MIRROR_BACKUP_DESTINATION_PATH";
+
+srcPaths = process.env[srcPathsEnvKey];
+if(typeof(srcPaths) == "undefined") {
+  throw Error("Environment variable %" + srcPathsEnvKey + "% is empty/absent");
+}
+srcPaths = srcPaths.split(PATH_SEPARATOR);
+
+dstPaths = process.env[dstPathsEnvKey];
+if(typeof(dstPaths) == "undefined") {
+  //throw Error("Environment variable %" + dstPathsEnvKey + "% is empty/absent");
+}
+
+srcPaths = srcPaths.map(function(item, index, array) {
+  return item.replace(/"/g, '');
+});
+console.log(srcPaths);
+
+
+//console.log(process.env.PATH.split(PATH_SEPARATOR));
+
+// var rsync = exec('rsync -v');
+// console.log(rsync.output);
 
 // set src=C:\Users\user0\Pictures
 // set dst=H:\the\root\of\the\backup\drives\C\Users\user0\Pictures

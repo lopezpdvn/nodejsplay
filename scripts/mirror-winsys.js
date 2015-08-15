3a34796085234b0e83204b1f201fae82b17545b4
Pedro Ivan Lopez <m@pedroivanlopez.com>
2015-08-15 02:17:26 -0500

    Use path delimiter constant provided by API instead of global var
    `path.delimiter`

--- scripts/mirror-winsys.js
+++ scripts/mirror-winsys.js
@@ -1,13 +1,14 @@
 #! /usr/bin/env shjs
 
 // Mirrors directories/files on Windows system.
+// Doesn't support paths that contain the character ';'
 
 require('shelljs/global');
 //yaml = require('js-yaml');
 var fs   = require('fs');
 var child_process = require('child_process');
+var path = require('path');
 var spawn = child_process.spawnSync;
-var PATH_SEPARATOR = ';';
 
 var srcPathsEnvKey = "SYSPOL_EXTERNAL_MIRROR_BACKUP_SOURCE_PATH";
 var dstPathsEnvKey = "SYSPOL_EXTERNAL_MIRROR_BACKUP_DESTINATION_PATH";
@@ -20,7 +21,7 @@ var paths = inData.map(function(item, index, array) {
   if(typeof(paths) == "undefined") {
     throw Error("Environment variable %" + item + "% is empty/absent");
   }
-  return paths.split(PATH_SEPARATOR);
+  return paths.split(new RegExp(path.delimiter, "g"));
 });
 
 // Strip double quotes
